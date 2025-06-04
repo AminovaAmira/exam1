8e72a434cc082f2e8139d0d0311b8ab1242862fa
Amira <vladilen.ivanov.04@mail.ru>
2025-06-04 16:18:18 +0300

    Создание страницы со списком предметов и списком сдающих эти экзамены

--- task/views.py
+++ task/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import AAExam
+from django.contrib.auth.models import User
 
-# Create your views here.
+def ipexam_list(request):
+    current_user = request.user
+
+    fio = f"{current_user.last_name} {current_user.first_name}" if current_user.is_authenticated else "Гость"
+    group = current_user.groups.first().name if current_user.is_authenticated and current_user.groups.exists() else "Группа не указана"
+    
+
+    exams = AAExam.objects.filter(is_public=True).prefetch_related('users')
+    
+    context = {
+        'exams': exams,
+        'fio': fio,
+        'group': group,
+    }
+    
+    return render(request, 'task/ipexam_list.html', context)
